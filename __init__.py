7afdc49b4129308ec31817c610ef08eb7271ea27
Smoodust <rustyburing@yandex.ru>
2023-03-23 17:13:19 +0400

    deleted args in invite_player

--- __init__.py
+++ __init__.py
@@ -12,6 +12,7 @@ def add_player():
     name = request.args.get('name')
     token = generate_unique_text(20, [x.token for x in players])
     players.append(Player(name, token))
+    app.logger.debug(players)
     return token
 
 @app.route("/lobby/")
@@ -19,14 +20,16 @@ def add_lobbys():
     token_creator = request.args.get('token_creator')
     code = generate_unique_text(5, [x.code for x in lobbys])
     lobbys.append(Lobby(code, [token_creator]))
+    app.logger.debug(lobbys)
     return code
 
 @app.route("/player/invite/")
-def invite_player(token, code):
+def invite_player():
     token = request.args.get('token')
     code = request.args.get('code')
     for lobby in lobbys:
         if lobby.code == code:
             lobby.members.append(token)
+            app.logger.debug(lobbys)
             return 'OK'
     return 'DONT FIND'
