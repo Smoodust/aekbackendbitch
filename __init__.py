254af17d1ebf23f3aad7502bcacc5df6e6f180c2
Smoodust <rustyburing@yandex.ru>
2023-03-23 17:06:04 +0400

    moved functions with flask (dont working)

--- __init__.py
+++ __init__.py
@@ -6,6 +6,22 @@ lobbys = []
 
 app = Flask(__name__)
 
-@app.route("/player/add")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/player/")
+def add_player(name):
+    token = generate_unique_text(20, [x.token for x in players])
+    players.append(Player(name, token))
+    return token
+
+@app.route("/lobby/")
+def add_lobbys(token_creator):
+    code = generate_unique_text(5, [x.code for x in lobbys])
+    lobbys.append(Lobby(code, [token_creator]))
+    return code
+
+@app.route("/player/invite/")
+def invite_player(token, code):
+    for lobby in lobbys:
+        if lobby.code == code:
+            lobby.members.append(token)
+            return 'OK'
+    return 'DONT FIND'
--- functions.py
+++ functions.py
@@ -10,20 +10,3 @@ def generate_unique_text(length, list_to_check):
     while text in [x for x in list_to_check]:
         text = randomword(length)
     return text
-
-def add_player(name):
-    token = generate_unique_text(20, [x.token for x in players])
-    players.append(Player(name, token))
-    return token
-
-def add_lobbys(token_creator):
-    code = generate_unique_text(5, [x.code for x in lobbys])
-    lobbys.append(Lobby(code, [token_creator]))
-    return code
-
-def invite_player(token, code):
-    for lobby in lobbys:
-        if lobby.code == code:
-            lobby.members.append(token)
-            return 'OK'
-    return 'DONT FIND'
