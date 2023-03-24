3e029f72823e66e4aa8200f6df7b2573fc18da22
Smoodust <rustyburing@yandex.ru>
2023-03-25 02:59:35 +0400

    commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { v4: uuidv4 } = require('uuid');
+let nanoid = import('nanoid');
 
 const express = require("express")
 const socketIo = require("socket.io")
@@ -12,19 +12,6 @@ const io = socketIo(server,{
     }
 })
 
-function getRandomName() {
-    let hexString = uuidv4();
-    console.log("hex:   ", hexString);
-    
-    // remove decoration
-    hexString = hexString.replace(/-/g, "");
-    
-    let base64String = Buffer.from(hexString, 'hex').toString('base64')
-    console.log("base64:", base64String);
-    
-    return base64String;
-}
-
 let players = {};
 let lobbys = {};
 
@@ -32,32 +19,38 @@ io.on("connection", (socket) => {
     console.log("client connected: ", socket.id);
 
     socket.on("create player", (nickname, callback) => {
-        let token = uuidv4();
-        players[token] = {
-            nickname: nickname,
-            token: token,
-            isActive: false
-        };
-        console.log("player created: ", nickname, " ", token);
-        callback({
-            status: "OK",
-            token: token
+        nanoid.then(module => {
+            let token = module.nanoid();
+            players[token] = {
+                nickname: nickname,
+                token: token,
+                isActive: false
+            };
+            console.log("player created: ", nickname, " ", token);
+            callback({
+                status: "OK",
+                token: token
+            });
         });
     });
 
     socket.on("create lobby", (callback) => {
-        let code = getRandomName();
-        lobbys[code] = {
-            status: "waiting",
-            code: code,
-            members: new Set()
-        };
-        callback({
-            status: "OK",
-            code: code
+        nanoid.then(module => {
+            let code = module.customAlphabet('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', 6)();
+            lobbys[code] = {
+                status: "waiting",
+                code: code,
+                membersA: new Set(),
+                membersB: new Set(),
+                minigames: ["guees", "arithmetic"]
+            };
+            callback({
+                status: "OK",
+                code: code
+            });
+    
+            console.log("lobby created: ", code);
         });
-
-        console.log("lobby created: ", code);
     });
 
     socket.on("join to room", (data, callback) => {
@@ -67,20 +60,67 @@ io.on("connection", (socket) => {
         if (token in players && code in lobbys)
         {
             socket.join(code);
-            lobbys[code].members.add(token);
+            if (lobbys[code].membersA.size > lobbys[code].membersB.size) {
+                lobbys[code].membersB.add(token);
+            } else {
+                lobbys[code].membersA.add(token);
+            }
 
-            callback({
-                status: "OK",
-                members: [...lobbys[code].members].map(x => players[x].nickname)
-            });
+            payload = {
+                membersA: [...lobbys[code].membersA].map(x => players[x].nickname),
+                membersB: [...lobbys[code].membersB].map(x => players[x].nickname)
+            }
+            socket.to(code).emit("members update", payload);
+            payload['status'] = 'OK';
+            callback(payload);
 
             console.log(token, " joined to ", code);
             console.log(lobbys);
         } else {
+            console.log(token in players, code in lobbys)
             callback({
                 status: "ERROR"
             });
         }
+    });
+
+    socket.on("set ready to game", (token, code) => {
+        players[token].isActive = true;
+        console.log(token, "is ready");
+        if ([...lobbys[code].membersA].reduce((accumulator, currentValue) => { return players[currentValue].isActive && accumulator; }, true) && [...lobbys[code].membersB].reduce((accumulator, currentValue) => { return players[currentValue].isActive && accumulator; }, true)) {
+            lobbys[code].status = "ready";
+            if (lobbys[code].minigames.length > 0)
+            {
+                socket.to(code).emit("ready to game", Date.now() + 3000);
+                socket.emit("ready to game", Date.now() + 3000);
+                setTimeout(() => {
+                    socket.to(code).emit("choose cards", {
+                        cards: lobbys[code].minigames,
+                        dateToEnd: Date.now() + 10000
+                    });
+                    socket.emit("choose cards", {
+                        cards: lobbys[code].minigames,
+                        dateToEnd: Date.now() + 10000
+                    });
+
+                }, 3000);
+            } else {
+                socket.to(code).emit("ready to scoreboard", Date.now() + 3000);
+                socket.emit("ready to scoreboard", Date.now() + 3000);
+                /*setTimeout(() => {
+                    console.log("win");
+                }, 3000);*/
+            }
+            console.log(token, "is ready");
+        }
+    });
+
+    socket.on("", (callback) => {
+        
+    });
+
+    socket.on("asd", (callback) => {
+        
     });
   
     socket.on("disconnect", (reason) => {
