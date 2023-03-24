5bfc4b4398308ed3fd3e88b821dfba360dddce36
Smoodust <rustyburing@yandex.ru>
2023-03-24 18:01:32 +0400

    intermediate result

--- __init__.py
+++ __init__.py
@@ -1,55 +1,24 @@
-from flask import Flask, request
-from flask_socketio import SocketIO, join_room, leave_room, emit
+import eventlet
+import socketio
 from functions import *
-from json import *
 
 players = {}
 lobbys = {}
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+sio = socketio.Server(cors_allowed_origins="*")
+app = socketio.WSGIApp(sio)
 
-@app.post("/player/")
-def add_player():
-    name = request.form.get('name')
-    token = generate_unique_text(20, [x.token for x in players])
-    players[token] = Player(name, token, False)
-    app.logger.debug(players)
-    return token
+@sio.event
+def connect(sid, environ):
+    print('connect ', sid)
 
-@app.post("/lobby/")
-def add_lobby():
-    code = generate_unique_text(5, [x.code for x in lobbys])
-    lobbys[code] = Lobby(code, LobbyStatus.waiting, [])
-    app.logger.debug(lobbys)
-    return code
+@sio.on('my custom event')
+def my_message(sid, data):
+    print('message ', data)
 
-@socketio.on('join')
-def on_join(data):
-    token = data['token']
-    code = data['code']
-    join_room(code)
-    emit('user_joined', {
-        'token':token,
-        'nickname':players[token].nickname,
-        'toRoom':code
-    }, to=code)
-    emit('lobby_members_info', [
-        players[member_token].nickname for member_token in lobbys.member
-    ])
-
-@socketio.on('leave')
-def on_leave(data):
-    token = data['token']
-    code = data['code']
-    leave_room(room)
-    players[token].isActive = False
-    emit('user_leaved', {
-        'token':token,
-        'nickname':players[token].nickname,
-        'toRoom':code
-    }, to=code)
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
